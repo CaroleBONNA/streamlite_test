78015e59a30a26c5843568c69af4b227c22083a8
CaroleBONNA <bonnardel.carole7@gmail.com>
2024-01-31 10:07:29 +0100

    test

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -2,16 +2,17 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import numpy as np
+import maplotlib.pyplot as plt
 
-st.title('Hello Wilders, welcome to my application!')
-st.write("I enjoy to discover streamlit possibilities")
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-df_weather = pd.read_csv(link)
-st.write(df_weather)
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
-viz_correlation = sns.heatmap(df_weather.select_dtypes('number').corr(),
-                              cmap="vlag")
-st.pyplot(viz_correlation.figure)
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ", name_length, "characters")
+st.title('Analyse de corrélation et de distribution')
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_car = pd.read_csv(link)
+st.write(df_car)
+
+
+#st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+#name = st.text_input("Please give me your name :")
+fig, ax = plt.subplots()
+sns.heatmap(df_car.select_dtypes('number').corr(), cmap="vlag", ax=ax)
+st.pyplot(fig)
