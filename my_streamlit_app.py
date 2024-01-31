f30d027bb3d88de0ee4b56de698ecf211152cc94
CaroleBONNA <bonnardel.carole7@gmail.com>
2024-01-31 11:46:34 +0100

    test

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -9,9 +9,28 @@ link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_car = pd.read_csv(link)
 st.write(df_car)
 
+#Liste des valeurs unique de la colonne continent 
+val_unique = df_car['continent'].unique()
 
-#st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+#Création de bouton de choix
+select_choice = st.multiselect("Selectionner un continent :", val_unique)
+
+#filtrer la df en fonction du continant selectionné
+
+df_car_filtre = df_car[df_car['continent'].isin(select_choice)]
+
+st.write("Corrélation")
+
 #name = st.text_input("Please give me your name :")
 fig, ax = plt.subplots()
-sns.heatmap(df_car.select_dtypes('number').corr(), cmap="vlag", ax=ax)
+sns.heatmap(df_car_filtre.select_dtypes('number').corr(), cmap="vlag", annot=True, ax=ax)
 st.pyplot(fig)
+
+st.write("Ananlyse du poids / puissance moteur ")
+
+st.scatter_chart(data=df_car_filtre, x='weightlbs', y='cubicinches')
+
+st.write("Ananlyse du poids / hp ")
+
+st.line_chart(data=df_car_filtre, x='weightlbs', y='hp')
+
